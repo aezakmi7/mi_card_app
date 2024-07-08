db4863ebd4151a4d37492bf83bfcc5cb26c7c6ac
Mykola Herasymenko <nicksuper2000@gmail.com>
2024-07-08 14:38:21 -0400

    card finished

--- lib/main.dart
+++ lib/main.dart
@@ -13,39 +13,81 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.amber,
         body: SafeArea(
           child: Center(
-              child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage('lib/assets/avatar.jpg'),
-              ),
-              Text(
-                'Mykola Herasymenko',
-                style: GoogleFonts.pacifico(
-                  textStyle: const TextStyle(
-                    fontSize: 25.0,
-                    color: Colors.white,
-                    fontWeight: FontWeight.normal,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('lib/assets/avatar.jpg'),
+                ),
+                Text(
+                  'Mykola Herasymenko',
+                  style: GoogleFonts.pacifico(
+                    textStyle: const TextStyle(
+                      fontSize: 25.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.normal,
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 5),
-              Text(
-                'Flutter Developer',
-                style: GoogleFonts.sourceCodePro(
-                  textStyle: const TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                    fontWeight: FontWeight.normal,
+                const SizedBox(height: 5),
+                Text(
+                  'Flutter Developer',
+                  style: GoogleFonts.sourceCodePro(
+                    textStyle: const TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.normal,
+                    ),
                   ),
                 ),
-              ),
-            ],
-          )),
+                const SizedBox(
+                  height: 20,
+                  width: 180,
+                  child: Divider(
+                    color: Colors.white,
+                  ),
+                ),
+                const Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.amber,
+                    ),
+                    title: Text(
+                      '+1 234 567 8910',
+                      style: TextStyle(
+                        color: Colors.amber,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                const Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.amber,
+                    ),
+                    title: Text(
+                      'mykola.herasymenko@gmail.com',
+                      style: TextStyle(
+                        color: Colors.amber,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
